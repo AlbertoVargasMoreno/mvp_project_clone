d6550f207d06cfa7007b5528288c378224d0ef64
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-08-10 11:04:11 -0400

    Creación de base de datos etapa inicial

---
+++ config/database/database.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS `mvp_project`;
+
+CREATE DATABASE IF NOT EXISTS `mvp_project`;
+
+USE `mvp_project`;
+
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+    `id`            tinyint(2)  unsigned NOT NULL AUTO_INCREMENT,
+    `name`          varchar(100)         NOT NULL,
+    `email`         varchar(100)         NOT NULL,
+    `password`      varchar(256)         NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `product` (
+    `id`            int(5)   unsigned NOT NULL AUTO_INCREMENT,
+    `description`   varchar(100)      NOT NULL,
+    `category`      varchar(100)      NOT NULL,
+    `enabled`       tinyint(1)        NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
