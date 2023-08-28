f09e1840b5fba9bc5310b711ddaee9e9e9883114
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-08-28 14:46:59 -0400

    Funcionalidad extra: buscador en index product

--- src/Views/product/index.php
+++ src/Views/product/index.php
@@ -1,6 +1,11 @@
 <h1>Productos</h1>
 <p>A list from all resources</p>
 
+<form action="" method="get">
+    <input type="text" name="q" placeholder="Buscar productos...">
+    <button type="submit">Buscar</button>
+</form>
+
 <ul>
     <?php foreach($data as $product): ?>
         <li><?= $product["description"] ?> | <a href='./show/<?= $product["id"] ?>'>Detalle</a> | <a href='./edit/<?= $product["id"] ?>'>Editar</a> | <a href='./destroy/<?= $product["id"] ?>'>Eliminar</a> </li>
