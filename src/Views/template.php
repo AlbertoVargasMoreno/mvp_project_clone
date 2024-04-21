61ff14cfc03b0edabe0805f65b3134771a397f7d
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-04-21 11:19:21 -0600

    fix: favicon not found

--- src/Views/template.php
+++ src/Views/template.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MVP Project</title>
+    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
 </head>
 <body>
     <?= $content; ?>
