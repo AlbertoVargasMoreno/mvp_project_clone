b91f6012e67adec1591df2f25a96c7a61af2822f
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-04-28 11:07:49 -0600

    feat: Improve security in home view by escaping out

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,8 +1,11 @@
-<?php session_start(); ?>
+<?php
+session_start();
+if (isset($_SESSION['name'])) $sanitizedName = htmlspecialchars($_SESSION['name']);
+?>
 <div class="col-8">
     <h1>Página Home</h1>
     <?php if (isset($_SESSION['name'])): ?>
-    <p>Bienvenid@ <?= $_SESSION['name']; ?></p>
+    <p>Bienvenid@ <?= $sanitizedName; ?></p>
     <?php endif; ?>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam numquam voluptates deleniti molestias quas facilis laboriosam quisquam tempora dolor autem!</p>
     <a href="product/">Products</a> | 
