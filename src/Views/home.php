e8306f55605e1221b54a7a631fd5ea3545bae0ad
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-05-01 11:53:31 -0600

    Refactor security features for home view
    - Remove escaping logic from home.php view
    - Follow security best practice of escaping late in the workflow rather than views
    - In UserController, escape name and email with htmlspecialchars() before setting session

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,11 +1,8 @@
-<?php
-session_start();
-if (isset($_SESSION['name'])) $sanitizedName = htmlspecialchars($_SESSION['name']);
-?>
+<?php session_start(); ?>
 <div class="col-8">
     <h1>Página Home</h1>
     <?php if (isset($_SESSION['name'])): ?>
-    <p>Bienvenid@ <?= $sanitizedName; ?></p>
+    <p>Bienvenid@ <?= $_SESSION['name']; ?></p>
     <?php endif; ?>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam numquam voluptates deleniti molestias quas facilis laboriosam quisquam tempora dolor autem!</p>
     <a href="product/">Products</a> | 
