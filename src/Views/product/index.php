fb69c7f39edc4e10441e58b45518200e0c8a966e
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-05-19 06:48:44 -0600

    Enhance UI and layout
    - Improve products index page structure and styling
    - Add 'Create New Product' button with improved positioning
    - Update navigation links for consistent user experience

--- src/Views/product/index.php
+++ src/Views/product/index.php
@@ -1,21 +1,31 @@
 <h1>Productos</h1>
 <p>A list from all resources</p>
 
-<form action="" method="get">
+<!-- <form action="" method="get">
     <input type="text" name="q" placeholder="Buscar productos...">
     <button type="submit">Buscar</button>
 </form>
+-->
 
-<ul>
+<a href="./create" class="button primary round icon-button expand-button">+ Nuevo producto</a> 
+
+<section class="cards">
     <?php foreach($data as $product): ?>
-        <li>
-            <?= $product["description"] ?> 
-            | <a href='./show/<?= $product["id"] ?>'>Detalle</a> 
-            | <a href='./edit/<?= $product["id"] ?>'>Editar</a> 
-            | <a href='./destroy/<?= $product["id"] ?>'>Eliminar</a>
-        </li>
+        <div class="card"> 
+            <img class="card-media" 
+            src="https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+             />
+            <div class="card-content">
+                <?= $product["description"] ?>
+                <div>
+                    | <a href='./show/<?= $product["id"] ?>' class="card-link">Detalle</a> 
+                    | <a href='./edit/<?= $product["id"] ?>' class="card-link">Editar</a> 
+                    | <a href='./destroy/<?= $product["id"] ?>' class="card-link">Eliminar</a>
+                </div>
+            </div>
+        </div>
     <?php endforeach; ?>
-</ul>
+</section>
 
-<a href="./create">+ Nuevo producto</a> |
+|
 <a href="../">Home</a>
--- src/Views/template.php
+++ src/Views/template.php
@@ -7,8 +7,15 @@
     <title>MVP Project</title>
     <link rel="icon" href="data:;base64,iVBORw0KGgo=">
     <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/normalize.slim.css">
+    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/base.css">
+    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/form.css">
+    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/card.css">
+    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/button.css">
+    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/index.css">
 </head>
 <body>
-    <?= $content; ?>
+    <main>
+        <?= $content; ?>
+    </main>
 </body>
 </html>
