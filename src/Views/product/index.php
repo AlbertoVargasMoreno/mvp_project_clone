9ff705621552f6e577e9a0620048312c79a41ac1
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-05-22 13:32:38 -0600

    WIP(productsView): define new layout

--- src/Views/product/index.php
+++ src/Views/product/index.php
@@ -1,33 +1,35 @@
-<h1>Productos</h1>
-<p>A list from all resources</p>
-
-<!-- <form action="" method="get">
-    <input type="text" name="q" placeholder="Buscar productos...">
-    <button type="submit">Buscar</button>
-</form>
-<div class="card">
-  <img class="card-media" src="https://picsum.photos/320/180" />
-  <div class="card-content">
-    <h5 class="card-title">Card title</h5>
-    <p>
-      Card text. Basic example of some text content inside a card. Wow, look at
-      this great text.
-    </p>
-    <button class="card-action primary">Card action</button>
-  </div>
-</div> -->
-
-<!-- src="https://loremflickr.com/320/180/gadget?random=2"  -->
-<!-- src="https://images.unsplash.com/photo-1618384887929-16ec33fab9ef?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -->
-
-<a href="./create" class="button primary round icon-button expand-button">+ Nuevo producto</a> 
+<section>
+  <h1>Productos</h1>
+  <p>A list from all resources</p>
+  
+  <!-- <form action="" method="get">
+      <input type="text" name="q" placeholder="Buscar productos...">
+      <button type="submit">Buscar</button>
+  </form>
+  <div class="card">
+    <img class="card-media" src="https://picsum.photos/320/180" />
+    <div class="card-content">
+      <h5 class="card-title">Card title</h5>
+      <p>
+        Card text. Basic example of some text content inside a card. Wow, look at
+        this great text.
+      </p>
+      <button class="card-action primary">Card action</button>
+    </div>
+  </div> -->
+  
+  <!-- src="https://loremflickr.com/320/180/gadget?random=2"  -->
+  <!-- src="https://images.unsplash.com/photo-1618384887929-16ec33fab9ef?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -->
+  
+  <a href="./create" class="button primary round icon-button expand-button">+ Nuevo producto</a>
+</section>
 
 <section class="cards">
     <?php foreach($data as $product): ?>
         <div class="card"> 
-            <img class="card-media" 
+            <!-- <img class="card-media" 
             src="https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-             />
+             /> -->
             <div class="card-content">
                 <?= $product["description"] ?>
                 <div>
@@ -40,5 +42,4 @@
     <?php endforeach; ?>
 </section>
 
-|
-<a href="../">Home</a>
+<a href="../">« Home</a>
--- src/Views/template.php
+++ src/Views/template.php
@@ -1,4 +1,8 @@
-<?php //session_start(); ?>
+<?php //session_start(); 
+    // var_dump($_SERVER['HTTP_HOST']);
+    // var_dump($_SERVER['REQUEST_URI']);
+    $serverHost = $_SERVER['HTTP_HOST'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +10,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MVP Project</title>
     <link rel="icon" href="data:;base64,iVBORw0KGgo=">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/normalize.slim.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/base.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/form.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/card.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/button.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/index.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/normalize.slim.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/base.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/form.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/card.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/button.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/index.css">
+    <!-- esta solucion no funciona, cuando se accede a traves de otro dispositivo, es necesario resolver como enlazar los public assets -->
 </head>
 <body>
     <main>
