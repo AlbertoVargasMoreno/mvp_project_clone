66fd988d1480f8acc490529456d378736bb2b0bc
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-05-22 13:32:38 -0600

    Dinamically generate CSS asset urls
    - Use $_SERVER['HTTP_HOST'] to create flexible asset links

--- src/Views/template.php
+++ src/Views/template.php
@@ -1,4 +1,6 @@
-<?php //session_start(); ?>
+<?php
+    $serverHost = $_SERVER['HTTP_HOST'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MVP Project</title>
     <link rel="icon" href="data:;base64,iVBORw0KGgo=">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/normalize.slim.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/base.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/form.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/card.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/button.css">
-    <link rel="stylesheet" href="http://localhost/mvp_project/public/assets/css/index.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/normalize.slim.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/base.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/form.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/card.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/button.css">
+    <link rel="stylesheet" href="http://<?=$serverHost;?>/mvp_project/public/assets/css/index.css">
 </head>
 <body>
     <main>
