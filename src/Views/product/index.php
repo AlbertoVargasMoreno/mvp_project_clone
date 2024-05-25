ddea5fecbdf9966c7cd7bd8e55942f3f667083ad
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-05-24 20:07:54 -0600

    Improve layout and navigation for products, login and home pages
    - Adjust button positioning in index.css
    - Restructure product index page
    - Implement consistent navigation with "Home" link on both pages

--- src/Views/product/index.php
+++ src/Views/product/index.php
@@ -1,20 +1,22 @@
-<h1>Productos</h1>
-<p>A list from all resources</p>
-
-<!-- <form action="" method="get">
-    <input type="text" name="q" placeholder="Buscar productos...">
-    <button type="submit">Buscar</button>
-</form>
--->
-
-<a href="./create" class="button primary round icon-button expand-button">+ Nuevo producto</a> 
+<section>
+  <a href="../">« Home</a>
+  <h1>Productos</h1>
+  <p>A list from all resources</p>
+  
+  <!-- <form action="" method="get">
+      <input type="text" name="q" placeholder="Buscar productos...">
+      <button type="submit">Buscar</button>
+  </form>
+  -->
+  
+<div class="flex container-create-btn">
+  <a href="./create" class="button primary round icon-button expand-button">+ Nuevo producto</a>
+</div>
+</section>
 
 <section class="cards">
     <?php foreach($data as $product): ?>
         <div class="card"> 
-            <img class="card-media" 
-            src="https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-             />
             <div class="card-content">
                 <?= $product["description"] ?>
                 <div>
@@ -27,5 +29,3 @@
     <?php endforeach; ?>
 </section>
 
-|
-<a href="../">Home</a>
--- src/Views/user/login.php
+++ src/Views/user/login.php
@@ -1,22 +1,28 @@
-<h1>Login</h1>
-<p>Please, login to manage the database</p> 
+<section>
+    <a href="../">« Home</a>
+</section>
 
-<form action="auth" method="post">
+<section>
+    <h1>Login</h1>
+    <p>Please, login to manage the database</p>
+</section> 
 
-    <div class="input-group">
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email">
-    </div>
-
-    <div class="input-group">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div class="button-container">
-        <button class="button-link" type="submit">Login</button>
-    </div>
-
-</form>
-
-<a href="../">Home</a>
+<section>
+    <form action="auth" method="post">
+    
+        <div class="input-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email">
+        </div>
+    
+        <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+    
+        <div class="button-container">
+            <button class="button-link" type="submit">Login</button>
+        </div>
+    
+    </form>
+</section>
